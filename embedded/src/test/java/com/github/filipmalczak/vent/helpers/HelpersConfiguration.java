65b320bb2f41fd3fc7b6ba6da86aeed92b8ece29
Filip Malczak <filip.malczak@sagiton.pl>
2018-08-10 20:51:59 +0200

    Removed legacy modules leftovers

--- embedded/src/test/java/com/github/filipmalczak/vent/helpers/HelpersConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.github.filipmalczak.vent.helpers;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class HelpersConfiguration {
-}
--- src/main/java/com/github/filipmalczak/vent/embedded/service/OptimizationManager.java
+++
@@ -1,4 +0,0 @@
-package com.github.filipmalczak.vent.mongo.service;
-
-public class OptimizationManager {
-}
